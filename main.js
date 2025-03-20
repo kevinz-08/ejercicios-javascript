fb66e8e0512d82f8d1d92d31176e23ffe6c872e9
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-20 08:32:50 -0500

    feat: :sparkles: de nuevo, error en el html ya que no se habia modificado lo que se habia cambio antes en el js

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-const numNormal = Number(prompt("Ingrese el Numero que desea exponenciar al cuadrado"))
+const num1 = Number (prompt("Ingrese el Numero 1"));
+const num2 = Number (prompt("Ingrese el Numero 2"));
+const signo = (prompt("Ingrese el signo con el que desea hacer la operacion (+,-,*,/): "));
 
-let res = numNormal **  2
 
-console.log(res);
+if (isNaN(num1) && isNaN(num2)){
+    alert("Ninguno de los valores colocados son reconocidos como numeros");
+    }
+
+else if (isNaN(num1)){
+    alert("El primer valor ingresado no es un numero");
+}
+else if (isNaN(num2)){
+    alert("El segundo valor ingresado no es un numero");
+}else{
+    if (signo == "+"){
+        let res = num1 + num2;
+        console.log(res);
+    }
+    
+    else if (signo == "-"){
+        let res = num1 - num2;
+        console.log(res);
+    }
+    else if (signo == "*"){
+        let res = num1 * num2;
+        console.log(res);
+    }
+    else if (signo == "/"){
+    
+    if (num2 != 0){
+        let res = num1 / num2;
+        console.log(res);
+    }
+    else {
+        alert("Este Numero no se puede dividir")
+    }
+    }
+    else {
+        alert("Este signo no es valido");
+    } 
+}
 

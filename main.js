30128361eb59d7c16845646af671318b37c2cdc6
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 07:00:15 -0500

    feat: :sparkles: Clasificación de Colores Primarios y Secundarios

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-let dia = prompt("Ingresa un dia de la semana (por ejemplo, 'Lunes'):");
- dia = dia.toLowerCase();
- switch (dia) {
-     case "lunes":
-         console.log("Es el inicio de la semana.");
-         break;
-     case "martes":
-         console.log("Ya es martes");
-         break;
-     case "miércoles":
-     case "miercoles": 
-         console.log("Mitad de semana.");
-         break;
-     case "jueves":
-         console.log("Ya casi es viernes, pero es jueves");
-         break;
-     case "viernes":
-         console.log("Por fin es viernes");
-         break;
-     case "sábado":
-     case "sabado":
-         console.log("El fin de semana ha llegado.");
-         break;
-     case "domingo":
-         console.log("Mañana empieza una nueva semana.");
-         break;
-     default:
-         console.log("Error: Ingresa un dia de la semana valido.");
-         break;
- }
+const clasificarColor = (color) => {
+    switch (color.toLowerCase()) {
+        case "rojo":
+        case "azul":
+        case "amarillo":
+            return `El color ${color} es primario.`;
+        
+        case "verde":
+        case "naranja":
+        case "violeta":
+            return `El color ${color} es secundario.`;
+        
+        default:
+            return "No es un color primario ni secundario.";
+    }
+};
+
+
+const colorIngresado = prompt("Ingresa un color:").trim(); 
+
+
+alert(clasificarColor(colorIngresado));
+console.log(clasificarColor(colorIngresado));

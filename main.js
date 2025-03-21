cf3d03f60527a4aac475d8cb6211d72cfab75f61
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 06:25:23 -0500

    feat: :sparkles: Rango de numeros

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-let peliculaElegida = prompt(
-    "Elige tu película favorita de la siguiente lista:\n" +
-    "1. Avengers EndGame\n" +
-    "2. El paseo 5\n" +
-    "3. Las esquinas de la mesa redonda\n" +
-    "4. Avatar\n" +
-    "5. Coco\n"
-);
-
-if (peliculaElegida < 1 ){
-    alert("La opción ingresada no es válida. Por favor, elige una opción correcta");
-}
-else if (peliculaElegida > 5 ){
-    alert("La opción ingresada no es válida. Por favor, elige una opción correcta");
-}else{
-    alert("Has elegido: " + peliculaElegida);
-    console.log("Película seleccionada:", peliculaElegida);
-}
-
+console.log("Numeros entre -20 y 30:");
+ for (let i = -20; i <= 30; i++) {
+     console.log(i);
+ }
+ console.log("Numeros pares entre 20 y 60:");
+ for (let i = 20; i <= 60; i++) {
+     if (i % 2 === 0) { 
+         console.log(i);
+     }
+ }
+ console.log("Numeros impares entre 600 y 666:");
+ for (let i = 600; i <= 666; i++) {
+     if (i % 2 !== 0) {
+         console.log(i);
+     }
+ }
+ console.log("Numeros primos entre 1 y 150:");
+ function esPrimo(numero) {
+     if (numero <= 1) return false;
+     for (let i = 2; i <= Math.sqrt(numero); i++) {
+         if (numero % i === 0) return false;
+     }
+     return true;
+ }
+ for (let i = 1; i <= 150; i++) {
+     if (esPrimo(i)) {
+         console.log(i);
+     }
+ }

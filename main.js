e16dcbe6a8732ec982dec9a96df6c68c6a2d0ad9
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 06:22:23 -0500

    feat: :sparkles: Comparación Estricta de Cadenas

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-let a = prompt("Ingresa el primer número:");
-let b = prompt("Ingresa el segundo número:");
-let c = prompt("Ingresa el tercer número:");
+let cadena1 = prompt("Ingrese la primera cadena de texto:");
+let cadena2 = prompt("Ingrese la segunda cadena de texto:");
 
-a = Number(a);
-b = Number(b);
-c = Number(c);
-
-if (a > b && a > c) {
-    alert("El número mayor es: " + a);
-} else if (b > a && b > c) {
-    alert("El número mayor es: " + b);
-}
-else if (c > a && c > b){
-    alert("El número mayor es: " + c);
-}else{
-    alert("Ingresa numeros validos, por favor");
+if (cadena1 === cadena2) {
+    console.log("Las cadenas son idénticas en valor y tipo");
+} else {
+    console.log("Las cadenas NO son idénticas en valor y/o tipo");
 }

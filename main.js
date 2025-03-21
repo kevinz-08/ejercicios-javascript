0fe76666b01f413d05eb1885a842e6db6c0d99ce
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 06:33:51 -0500

    feat: :sparkles: Sistema de Calificaciones

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-let temperatura = parseFloat(prompt("Ingresa la temperatura en grados Celsius:"));
-
-
-if (temperatura <= 15) {
-     console.log("El clima es frío.");
-}else if (temperatura >= 16 && temperatura <= 25) {
-     console.log("El clima es templado.");
-}else {
-     console.log("El clima es caluroso.");
+function convertirCalificacion(calificacion) {
+    if (calificacion >= 90 && calificacion <= 100) {
+        return "A";
+    } else if (calificacion >= 80 && calificacion < 90) {
+        return "B";
+    } else if (calificacion >= 70 && calificacion < 80) {
+        return "C";
+    } else if (calificacion >= 60 && calificacion < 70) {
+        return "D";
+    } else if (calificacion >= 0 && calificacion < 60) {
+        return "F";
+    } else {
+        return "Calificación no válida";
+    }
 }
+let calificacionNumerica = parseFloat(prompt("Ingresa tu calificación (0-100):"));
+let calificacionLetra = convertirCalificacion(calificacionNumerica);
+console.log("Tu calificación es: " + calificacionLetra);

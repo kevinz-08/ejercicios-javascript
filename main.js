ad6b7fa8dbe214ef9790059d7b78fb417ab7eeed
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 06:36:53 -0500

    feat: :sparkles: Verificador de Acceso Seguro

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-function convertirCalificacion(calificacion) {
-    if (calificacion >= 90 && calificacion <= 100) {
-        return "A";
-    } else if (calificacion >= 80 && calificacion < 90) {
-        return "B";
-    } else if (calificacion >= 70 && calificacion < 80) {
-        return "C";
-    } else if (calificacion >= 60 && calificacion < 70) {
-        return "D";
-    } else if (calificacion >= 0 && calificacion < 60) {
-        return "F";
-    } else {
-        return "Calificación no válida";
-    }
-}
-let calificacionNumerica = parseFloat(prompt("Ingresa tu calificación (0-100):"));
-let calificacionLetra = convertirCalificacion(calificacionNumerica);
-console.log("Tu calificación es: " + calificacionLetra);
+const contraseñaCorrecta = "seguro123";
+
+let edad = parseInt(prompt("Ingresa tu edad:"));
+let contrasena = prompt("Ingresa la contraseña:");
+
+ if (edad > 18 && contrasena === contraseñaCorrecta) {
+     console.log("Acceso concedido");
+ } else {
+     console.log("Acceso denegado, verifica tu edad o contraseña.");
+ }

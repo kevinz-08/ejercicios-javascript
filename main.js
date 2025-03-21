27726bacdb69fd8933522f0cc8615f3e9feebc2e
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 07:02:06 -0500

    feat: :sparkles: Conversión de Número a Mes del Año

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-const clasificarColor = (color) => {
-    switch (color.toLowerCase()) {
-        case "rojo":
-        case "azul":
-        case "amarillo":
-            return `El color ${color} es primario.`;
-        
-        case "verde":
-        case "naranja":
-        case "violeta":
-            return `El color ${color} es secundario.`;
-        
-        default:
-            return "No es un color primario ni secundario.";
-    }
-};
-
-
-const colorIngresado = prompt("Ingresa un color:").trim(); 
-
-
-alert(clasificarColor(colorIngresado));
-console.log(clasificarColor(colorIngresado));
+let numero = Number(prompt("Ingresa un numero del 1 al 12:"));
+ switch (numero) {
+     case 1:
+         console.log("Enero");
+         break;
+     case 2:
+         console.log("Febrero");
+         break;
+     case 3:
+         console.log("Marzo");
+         break;
+     case 4:
+         console.log("Abril");
+         break;
+     case 5:
+         console.log("Mayo");
+         break;
+     case 6:
+         console.log("Junio");
+         break;
+     case 7:
+         console.log("Julio");
+         break;
+     case 8:
+         console.log("Agosto");
+         break;
+     case 9:
+         console.log("Septiembre");
+         break;
+     case 10:
+         console.log("Octubre");
+         break;
+     case 11:
+         console.log("Noviembre");
+         break;
+     case 12:
+         console.log("Diciembre");
+         break;
+     default:
+         console.log("Ingrese un nu4mero válido del 1 al 12.");
+         break;
+ }

e335bf320eae6d0ccda77b95e70d6a490f045abd
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-03-21 00:01:35 -0500

    feat: :sparkles: Determinar el Mayor de Tres Números

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-let numero1 = Number(prompt("Ingresa el primer número:"));
-let numero2 = Number(prompt("Ingresa el segundo número:"));
+let a = prompt("Ingresa el primer número:");
+let b = prompt("Ingresa el segundo número:");
+let c = prompt("Ingresa el tercer número:");
 
-const compararNumeros = function (num1, num2) {
-    if (num1 > num2) {
-        return `El número ${num1} es mayor que ${num2}`;
-    } else if (num1 < num2) {
-        return `El número ${num1} es menor que ${num2}`;
-    } else {
-        return `El número ${num1} es igual a ${num2}`;
-    }
-};
+a = Number(a);
+b = Number(b);
+c = Number(c);
 
-alert(compararNumeros(numero1, numero2));
-console.log(compararNumeros(numero1, numero2));
+if (a > b && a > c) {
+    alert("El número mayor es: " + a);
+} else if (b > a && b > c) {
+    alert("El número mayor es: " + b);
+}
+else if (c > a && c > b){
+    alert("El número mayor es: " + c);
+}else{
+    alert("Ingresa numeros validos, por favor");
+}
